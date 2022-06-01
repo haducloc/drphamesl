3b2f8d96bb17f28d6e8c9128c0e95bda52b8d348
loc.ha <loc.ha@GPCIT2UA7371TV9.stone.ne.gov>
2022-06-01 09:36:34 -0500

    Add TestController class

---
+++ src/main/java/com/drphamesl/admin/controllers/TestController.java
@@ -0,0 +1,33 @@
+package com.drphamesl.admin.controllers;
+
+import java.time.OffsetDateTime;
+
+import com.appslandia.plum.base.Controller;
+import com.appslandia.plum.base.EnableAsync;
+import com.appslandia.plum.base.Modules;
+import com.drphamesl.entities.Account;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+/**
+ *
+ * @author <a href="mailto:haducloc13@gmail.com">Loc Ha</a>
+ *
+ */
+@ApplicationScoped
+@Controller(module = Modules.ADMIN)
+public class TestController {
+
+	@EnableAsync
+	public Account index() {
+		Account acc = new Account();
+		acc.setAccountId(1);
+		acc.setEmail("test@gmail.com");
+		acc.setFirstName("Loc");
+		acc.setLastName("Ha");
+		acc.setStatus(1);
+		acc.setTimeCreated(OffsetDateTime.now());
+
+		return acc;
+	}
+}
