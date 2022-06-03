b855977a4a0a0e378a5d33ee60d52bd4dbcfd9bf
loc.ha <loc.ha@GPCIT2UA7371TV9.stone.ne.gov>
2022-06-03 15:20:19 -0500

    Minor changes

--- src/main/java/com/drphamesl/beans/ErrorServletImpl.java
+++ src/main/java/com/drphamesl/beans/ErrorServletImpl.java
@@ -1,15 +1,11 @@
 package com.drphamesl.beans;
 
-import java.io.IOException;
-
 import com.appslandia.plum.base.ErrorServlet;
 import com.appslandia.plum.base.Modules;
 import com.appslandia.plum.base.RequestContext;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
@@ -29,10 +25,5 @@ public class ErrorServletImpl extends ErrorServlet {
 		}
 
 		return "/error.jsp";
-	}
-
-	@Override
-	protected void writeErrorDev(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		writeErrorProd(request, response);
 	}
 }
